da238be3cbc422cc50dc802d76b07871631a84df
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-04-25 09:49:09 +0600

    1st commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { HomeScreen } from '@/common/screens/home'
+
+export default function Home() {
+  return <HomeScreen />
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/common/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
